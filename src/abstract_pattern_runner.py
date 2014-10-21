d52399a52957ea0765d0e20502a6016d65f2ba4c
David Osumi-Sutherland <dosumis@gmail.com>
2014-10-21 10:15:33 +0100

    abstract pattern runner works from args & is more chatty

--- src/abstract_pattern_runner.py
+++ src/abstract_pattern_runner.py
@@ -1,11 +1,16 @@
+#!/usr/bin/env jython -J-Xmx8000m
+
 import json
 import pattern
 import glob
 import re
 from uk.ac.ebi.brain.core import Brain
+import sys
 
 
-"""Project specific runner script. Runs verification tests on abstract patterns and generates markdown docs"""
+"""Runs verification tests on abstract patterns and generates markdown docs.
+1st ARG specifies path to input/output files.  
+Second ARG specifies ontology file to use in validation."""
 
 def load_json_from_file(path):
     json_file = open(path, "r")
@@ -15,17 +20,18 @@ def load_json_from_file(path):
 
 # Testing abstract pattern validation and documentation
 o = Brain()
-#o.learn("file:///repos/go_trunk_ont/extensions/go-plus.owl") # Running with local file for now.
-
-json_files = glob.glob("../patterns/*.json")  # Note - glob returns full file path
+o.learn(sys.argv[2]) # Running with local file for now.
+# Switch this to specify path as argv
+json_files = glob.glob(sys.argv[1] + "*.json")  # Note - glob returns full file path
 
 for f in json_files:
     p = load_json_from_file(f)
     m = re.search("(.+).json", f)
     pattern_name = m.group(1)
+    print "Processing %s" % pattern_name
     ap = pattern.abstract_pattern(p, o)
     md = open(pattern_name + ".md", "w")
-    print ap.gen_markdown_doc()
+    #print ap.gen_markdown_doc()
     md.write(ap.gen_markdown_doc())
     md.close()
     o.sleep()
