af4cd2917bd2d3b685b7d445da39c2c0ae4bef43
David Osumi-Sutherland <dosumis@gmail.com>
2016-11-11 14:36:48 +0000

    Update test_schema.py

--- spec/test_schema.py
+++ spec/test_schema.py
@@ -2,12 +2,15 @@ import yaml
 import json
 from jsonschema import Draft4Validator
 
-test_positive_file = open("test/postive_test.yaml", "w")
-
-test_positive = yaml.loads(test_positive_file)
-
+dosdp_core_file = open("DOSDP_schema_core.yaml")
+dosdp_core = yaml.loads(dosdp_core_file)
 
 v = Draft4Validator(dosdp_core)
+
+
+test_positive_file = open("test/postive_test.yaml", "w")
+test_positive = yaml.loads(test_positive_file)
+
 if not v.is_valid(test_positive):
     es = v.iter_errors(test_positive)
     for e in es: print(e)
