c181f9243da45fef05ee43120c4b1ab542f4704c
David Osumi-Sutherland <dosumis@gmail.com>
2017-02-15 12:28:05 +0000

    now tests OBO (via merging schemas in script, rather than import)

--- spec/test_schema.py
+++ spec/test_schema.py
@@ -1,21 +1,40 @@
 import yaml
 from jsonschema import Draft4Validator
-import sys
+import warnings
 
 dosdp_core_file = open("DOSDP_schema_core.yaml", "r")
 dosdp_core = yaml.load(dosdp_core_file.read())
+dosdp_obo_file = open("DOSDP_OBO_fields.yaml", "r")
+dosdp_obo = yaml.load(dosdp_obo_file.read())
 
-v = Draft4Validator(dosdp_core)
+dosdp = {}
+dosdp.update(dosdp_core)
+dosdp.update(dosdp_obo)
 
+def test_jschema(validator, file_path):
+    test_file = open(file_path, "r")
+    test_pattern = yaml.load(test_file.read())
 
-test_positive_file = open("test/test_positive.yaml", "r")
-test_positive = yaml.load(test_positive_file.read())
+    if not validator.is_valid(test_pattern):
+        es = validator.iter_errors(test_pattern)
+        for e in es:
+            warnings.warn(e)
+            return False
+    else:
+        return True
 
-if not v.is_valid(test_positive):
-    es = v.iter_errors(test_positive)
-    for e in es: 
-        print(e)
+V = Draft4Validator(dosdp)
+stat = True
+
+# Test core - positive
+if not test_jschema(V, "test/test_positive.yaml"): stat = False
+# Test obo - positive
+if not test_jschema(V, "../patterns/import_across_membrane.yaml"): stat = False
+
+if not stat:
     sys.exit(1)
-    
-else:
-    print(True)
+
+
+
+# TODO - add some negative tests - designed to fail to test aspects of spec.
+
