7a7b80247745120430a84c27cbf5dc08aa153f9b
hkir-dev <huseyin_kir@yahoo.com>
2021-07-06 13:20:02 +0100

    #75 os.path.splitext crashed in the server (docker?)

--- src/dosdp/document/document.py
+++ src/dosdp/document/document.py
@@ -34,7 +34,7 @@ def generate_pattern_documentation(yaml_location, md_location=None, sample_data_
     """
     logging.info("Documenting pattern file: " + yaml_location)
     if md_location is None:
-        md_location = os.path.splitext(yaml_location)[0] + ".md"
+        md_location = str(yaml_location).split(".yaml")[0] + ".md"
 
     if os.path.isdir(yaml_location):
         if not yaml_location.endswith(os.path.sep):
