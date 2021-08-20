3553b15a57131a8659584517722e0b5aa03cfb85
hkir-dev <huseyin_kir@yahoo.com>
2021-08-20 18:56:20 +0100

    0 to many support added to validator

--- src/dosdp/validator.py
+++ src/dosdp/validator.py
@@ -33,7 +33,6 @@ def test_jschema(validator, pattern):
     return is_valid
 
 
-
 def test_vars(pattern):
     """
     Tests whether variable names in any field with key 'vars' is in the vars list for the pattern
@@ -47,8 +46,12 @@ def test_vars(pattern):
     else:
         warnings.warn("Pattern has no vars")
         return True ## If this is to be compulsory, should be spec'd as such in json_schema
+    if 'list_vars' in pattern.keys():
+        vars.update(set(pattern['list_vars'].keys()))
     if 'data_vars' in pattern.keys():
         vars.update(set(pattern['data_vars'].keys()))
+    if 'data_list_vars' in pattern.keys():
+        vars.update(set(pattern['data_list_vars'].keys()))
     if 'substitutions' in pattern.keys():
         subvars = [X['out'] for X in pattern['substitutions']]
         vars.update(set(subvars))
@@ -83,7 +86,11 @@ def test_text_fields(pattern):
     if 'relations' in pattern.keys(): owl_entities.update(set(pattern['relations'].keys()))
     expr = parse('logical_axioms.[*].text')
     ms_fields = [match for match in expr.find(pattern)]
+    expr = parse('logical_axioms.[*].multi_clause.clauses.[*].text')
+    ms_fields.extend(match for match in expr.find(pattern))
     expr = parse('equivalentTo|subClassOf|GCI|disjointWith.text')
+    ms_fields.extend([match for match in expr.find(pattern)])
+    expr = parse('equivalentTo|subClassOf|GCI|disjointWith.multi_clause.clauses.[*].text')
     ms_fields.extend([match for match in expr.find(pattern)])
     stat=True
     if ms_fields:
@@ -103,6 +110,93 @@ def test_text_fields(pattern):
                 stat = False
     else:
         warnings.warn("Pattern has no text fields")
+    return stat
+
+
+def test_clause_nesting(pattern):
+    """
+     Structurally tests that logical axioms and annotations don't use nested multi_clause structures.
+     Only one level of multi_clause allowed (clauses of the multi_clause cannot have sub_clauses).
+     Args:
+         pattern: schema in yaml format to validate
+
+    Returns: True if multi_clause nesting is valid, False otherwise.
+    """
+    expr = parse('logical_axioms.[*].multi_clause.clauses.[*].sub_clauses')
+    ms_fields = [match for match in expr.find(pattern)]
+    expr = parse('equivalentTo|subClassOf|GCI|disjointWith.multi_clause.clauses.[*].sub_clauses')
+    ms_fields.extend([match for match in expr.find(pattern)])
+
+    stat = True
+    if ms_fields:
+        stat = False
+        warnings.warn("Detected multi_clause nesting in expressions. "
+                      "Logical axioms and annotations cannot have sub_clauses.")
+    return stat
+
+
+def test_axiom_separator(pattern):
+    """
+     Structurally tests that logical axioms can only have ' and ' and ' or ' as value.
+     Args:
+         pattern: schema in yaml format to validate
+
+    Returns: True if axiom separators are valid, False otherwise.
+    """
+    expr = parse('logical_axioms.[*].multi_clause.sep')
+    ms_fields = [match for match in expr.find(pattern)]
+    expr = parse('equivalentTo|subClassOf|GCI|disjointWith.multi_clause.sep')
+    ms_fields.extend([match for match in expr.find(pattern)])
+
+    stat = True
+    if ms_fields:
+        for field in ms_fields:
+            val = field.value
+            if val != " and " and val != " or ":
+                warnings.warn("Logical axioms can only have ' and ' and ' or ' as value. '%s' is not a valid value." % val)
+                stat = False
+    return stat
+
+
+def test_multi_clause_multi_list(pattern):
+    """
+     Multi_clauses can have at most one list variable.
+     Args:
+         pattern: schema in yaml format to validate
+
+    Returns: True if multi_clause list variable usage is valid, False otherwise.
+    """
+    list_vars = set()
+    if 'list_vars' in pattern.keys():
+        list_vars.update(pattern['list_vars'].keys())
+    if 'data_list_vars' in pattern.keys():
+        list_vars.update(set(pattern['data_list_vars'].keys()))
+
+    expr = parse('$..multi_clause')
+    multi_clause_matches = [match for match in expr.find(pattern)]
+    multi_clauses = [str(i.full_path) for i in multi_clause_matches]
+
+    mc_vars = {}
+    for mc in multi_clauses:
+        mc_vars[mc] = list()
+
+    expr = parse('$..multi_clause..vars')
+    var_fields = [match for match in expr.find(pattern)]
+
+    for field in var_fields:
+        var_path = str(field.full_path)
+        parent_mc = str(var_path[0: (var_path.find("multi_clause") + len("multi_clause"))])
+        new_list = list(mc_vars[parent_mc])
+        new_list.extend(list(field.value))
+        mc_vars[parent_mc] = new_list
+
+    stat = True
+    for mc_var in mc_vars:
+        list_references = [var for var in mc_vars[mc_var] if var in list_vars]
+        if len(list_references) > 1:
+            warnings.warn("Multi_clauses can have at most one list variable. But has multiple list references: %s"
+                          % str(list_references))
+            stat = False
     return stat
 
 
@@ -152,6 +246,9 @@ def validate(pattern):
                 if not test_jschema(validator, pattern): stat = False
                 if not test_vars(pattern): stat = False
                 if not test_text_fields(pattern): stat = False
+                if not test_clause_nesting(pattern): stat = False
+                if not test_axiom_separator(pattern): stat = False
+                if not test_multi_clause_multi_list(pattern): stat = False
             except YAMLError as exc:
                 stat = False
                 logging.error('Failed to load pattern file: ' + pattern_doc)
--- src/schema/test/generic_test/validator_test.py
+++ src/schema/test/generic_test/validator_test.py
@@ -18,6 +18,16 @@ NEGATIVE_PATTERN_UNDECLARED_CLASS = os.path.join(os.path.dirname(os.path.realpat
                                                  "../negative_test_set/undeclared_class.yaml")
 NEGATIVE_PATTERN_UNDECLARED_REL = os.path.join(os.path.dirname(os.path.realpath(__file__)),
                                                "../negative_test_set/undeclared_relation.yaml")
+NEGATIVE_PATTERN_UNDECLARED_REL_MC = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                                                  "../negative_test_set/undeclared_relation_multi_clause.yaml")
+NEGATIVE_PATTERN_AXIOM_CLAUSE_NESTING = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                                                     "../negative_test_set/axiom_multi_clause_nesting.yaml")
+NEGATIVE_PATTERN_AXIOM_SEPARATOR = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                                                "../negative_test_set/axiom_separator.yaml")
+NEGATIVE_PATTERN_MULTI_CLAUSE_MULTI_LIST = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                                                        "../negative_test_set/multi_clause_with_multi_list.yaml")
+NEGATIVE_PATTERN_MULTI_CLAUSE_MULTI_LIST2 = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                                                         "../negative_test_set/multi_clause_with_multi_list2.yaml")
 
 
 class ValidatorTest(unittest.TestCase):
@@ -39,4 +49,14 @@ class ValidatorTest(unittest.TestCase):
 
     def test_undeclared_relation(self):
         self.assertFalse(validate(NEGATIVE_PATTERN_UNDECLARED_REL))
+        self.assertFalse(validate(NEGATIVE_PATTERN_UNDECLARED_REL_MC))
 
+    def test_axiom_clause_nesting(self):
+        self.assertFalse(validate(NEGATIVE_PATTERN_AXIOM_CLAUSE_NESTING))
+
+    def test_axiom_separator(self):
+        self.assertFalse(validate(NEGATIVE_PATTERN_AXIOM_SEPARATOR))
+
+    def test_single_list_per_multi_clause(self):
+        self.assertFalse(validate(NEGATIVE_PATTERN_MULTI_CLAUSE_MULTI_LIST))
+        self.assertFalse(validate(NEGATIVE_PATTERN_MULTI_CLAUSE_MULTI_LIST2))
