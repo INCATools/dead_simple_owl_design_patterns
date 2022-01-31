4062875b88ef867e6835747b561e5f948255bdee
hkir-dev <huseyin_kir@yahoo.com>
2022-01-31 15:34:03 +0000

    New method request: dosdp normalise #80

--- src/dosdp/__main__.py
+++ src/dosdp/__main__.py
@@ -1,10 +1,10 @@
 import argparse
 import sys
 import pathlib
-import os
 import logging
 from dosdp import validator
 from dosdp.document import document
+from dosdp.normalise import normalizer
 
 logging.basicConfig(level=logging.INFO)
 
@@ -13,19 +13,9 @@ def main():
     parser = argparse.ArgumentParser(prog="dosdp", description='DOS-DP exchange format cli interface.')
     subparsers = parser.add_subparsers(help='Available dosdp actions', dest='action')
 
-    parser_validate = subparsers.add_parser("validate",
-                                            description="The validate parser",
-                                            help="Validates given argument if it is a yaml/yaml file. If argument is a "
-                                                 "folder, validates all pattern files located in the given directory.")
-    parser_validate.add_argument('-i', '--input', action='store', type=pathlib.Path, required=True)
-
-    parser_document = subparsers.add_parser("document", add_help=False,
-                                            description="The document generation parser",
-                                            help="Generates documentation for the given YAML schema.")
-    parser_document.add_argument('-s', '--schema', action='store_true')
-    parser_document.add_argument('-i', '--input', action='store', type=pathlib.Path)
-    parser_document.add_argument('-o', '--output', action='store', type=pathlib.Path)
-    parser_document.add_argument('-d', '--data', action='store', type=pathlib.Path)
+    add_validate_parser(subparsers)
+    add_document_parser(subparsers)
+    add_normalise_parser(subparsers)
 
     args = parser.parse_args()
 
@@ -41,6 +31,34 @@ def main():
         else:
             logging.error("Please use '--schema' to generate schema documentation "
                           "or '--input' for pattern documentation.")
+    elif args.action == "normalise":
+        normalizer.normalise(args.input, args.output)
+
+
+def add_normalise_parser(subparsers):
+    parser_normalise = subparsers.add_parser("normalise", add_help=False,
+                                             description="The template normalisation parser",
+                                             help="Normalises format of the given DOSDP schema.")
+    parser_normalise.add_argument('-i', '--input', action='store', type=pathlib.Path, required=True)
+    parser_normalise.add_argument('-o', '--output', action='store', type=pathlib.Path)
+
+
+def add_document_parser(subparsers):
+    parser_document = subparsers.add_parser("document", add_help=False,
+                                            description="The document generation parser",
+                                            help="Generates documentation for the given YAML schema.")
+    parser_document.add_argument('-s', '--schema', action='store_true')
+    parser_document.add_argument('-i', '--input', action='store', type=pathlib.Path)
+    parser_document.add_argument('-o', '--output', action='store', type=pathlib.Path)
+    parser_document.add_argument('-d', '--data', action='store', type=pathlib.Path)
+
+
+def add_validate_parser(subparsers):
+    parser_validate = subparsers.add_parser("validate",
+                                            description="The validate parser",
+                                            help="Validates given argument if it is a yaml/yaml file. If argument is a "
+                                                 "folder, validates all pattern files located in the given directory.")
+    parser_validate.add_argument('-i', '--input', action='store', type=pathlib.Path, required=True)
 
 
 if __name__ == "__main__":
---
+++ src/schema/test/generic_test/normalizer_test.py
@@ -0,0 +1,93 @@
+import unittest
+import os
+from pathlib import Path
+
+
+from dosdp.normalise import normalizer
+
+WRONG_ORDER_1 = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                                  "../normalisation_test_set/wrong_order1.yaml")
+WRONG_INDENT = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                                  "../normalisation_test_set/wrong_indentation.yaml")
+WRONG_SPACING = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                                  "../normalisation_test_set/wrong_spacing.yaml")
+NORMALISED_OUTPUT = Path(os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                                      "../normalisation_test_set/normalised_output.yaml"))
+
+
+class NormalizerTest(unittest.TestCase):
+
+    # def tearDown(self):
+    def setUp(self):
+        """
+        Delete files generated during tests
+        """
+        if os.path.exists(NORMALISED_OUTPUT):
+            os.remove(NORMALISED_OUTPUT)
+
+    def test_order_normalisation(self):
+        normalizer.normalise(WRONG_ORDER_1, NORMALISED_OUTPUT)
+        print(NORMALISED_OUTPUT)
+        output = normalizer.read_yaml_file(NORMALISED_OUTPUT)
+        expected_order = ["pattern_name", "pattern_iri", "contributors", "description", "classes", "relations",
+                          "annotationProperties", "vars", "annotations", "equivalentTo", "name", "def"]
+
+        self.assertEqual(list(output.keys()), expected_order)
+
+    def test_sub_element_order_normalisation(self):
+        normalizer.normalise(WRONG_ORDER_1, NORMALISED_OUTPUT)
+        print(NORMALISED_OUTPUT)
+        output = normalizer.read_yaml_file(NORMALISED_OUTPUT)
+
+        self.assertEqual(list(output["equivalentTo"].keys()), ["text", "vars"])
+        self.assertEqual(list(output["name"].keys()), ["text", "vars"])
+        self.assertEqual(list(output["annotations"][0].keys()), ["annotationProperty", "text", "vars"])
+
+        # do not edit classes
+        self.assertEqual(list(output["classes"].keys()), ["acute", "disease"])
+
+    def test_indentation_normalisation(self):
+        normalizer.normalise(WRONG_INDENT, NORMALISED_OUTPUT)
+        output = open(NORMALISED_OUTPUT, 'r')
+        lines = output.readlines()
+
+        previous_indentation = 0
+        line_number = 1
+        for line in lines:
+            if line.strip():
+                leading_spaces = len(line) - len(line.lstrip())
+                if not (leading_spaces == 0 or
+                        leading_spaces == previous_indentation or
+                        leading_spaces == previous_indentation + 2 or
+                        leading_spaces == previous_indentation - 2):
+                    print("leading spaces: " + str(leading_spaces))
+                    print("previous indent: " + str(previous_indentation))
+                    self.fail("Wrong indentation at line: " + str(line_number) + " '" + line + "'")
+                previous_indentation = leading_spaces
+            else:
+                # empty line
+                previous_indentation = 0
+            line_number += 1
+
+        output.close()
+
+    def test_spacing_normalisation(self):
+        normalizer.normalise(WRONG_SPACING, NORMALISED_OUTPUT)
+        output = open(NORMALISED_OUTPUT, 'r')
+        lines = output.readlines()
+
+        previous_line = "\n"
+        line_number = 1
+        for line in lines:
+            if line.strip():
+                leading_spaces = len(line) - len(line.lstrip())
+                if leading_spaces == 0 and not line.startswith("- "):
+                    # there should be an empty line before every top level element
+                    print(line)
+                    self.assertEqual(previous_line, "\n")
+            elif not previous_line.strip():
+                self.fail("Consecutive empty lines at line: " + str(line_number) + " '" + line + "'")
+            previous_line = line
+            line_number += 1
+
+        output.close()
